3b21c4c8e506db9b86cb6e74622b1fec5f24d702
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2019-04-21 23:45:14 +0300

    Add main file with simple window.

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+
+class Project: # Model class
+    pass
+
+class View:
+    pass
+
+class Controller:
+    pass
+
+if __name__ == "__main__":
+    app = tkinter.Frame()
+    app.mainloop()
