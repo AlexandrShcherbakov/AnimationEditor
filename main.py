2b2ccf2b07a54e640201bc44f8498126d0ea1da4
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2019-04-22 23:27:08 +0300

    Add file menu

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ class MainWindow(tkinter.Tk):
     def __init__(self):
         tkinter.Tk.__init__(self)
         self.title("Animation creator")
+        self._init_menu()
+
+    def _init_menu(self):
+        self.main_menu = tkinter.Menu(self)
+        self.config(menu=self.main_menu)
+
+        file_menu = tkinter.Menu(self.main_menu)
+        file_menu.add_command(label="Open Project")
+        file_menu.add_command(label="Save Project")
+        file_menu.add_command(label="Exit", command=self.quit)
+        self.main_menu.add_cascade(label="File", menu=file_menu)
 
 if __name__ == "__main__":
     app = MainWindow()
